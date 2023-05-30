313a5ab4298663f2da1345b73b532bb0cc75316d
ED <edoardo.deirossi@student.h-is.com>
2023-05-30 17:56:19 +0200

    YES

--- Login/model/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-const account = new Schema({
-    username: String,
-    email: String,
-    password: String,
-    recentAuth: Date
-});
-mongoose.model('users', account);
