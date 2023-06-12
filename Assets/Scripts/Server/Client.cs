67ec58d865a7af26ccb0397dbef3ba3a607d1053
ED <edoardo.deirossi@student.h-is.com>
2023-06-12 21:25:12 +0200

    Good Job Server
    SERVER + CLIENT NETMESSAGES

--- Assets/Scripts/Server/Client.cs
+++ Assets/Scripts/Server/Client.cs
@@ -1,18 +1,112 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Networking.Transport;
 using UnityEngine;
 
 public class Client : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Client Instance { set; get; }
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public NetworkDriver NetDrive;
+    private NetworkConnection SingularConnection;
+    private bool ServerActive = false;
+    public Action Drop;
+
+    public void __init__(string IP_ADDRESS, ushort PORT)
     {
-        
+        NetDrive = NetworkDriver.Create();
+        NetworkEndPoint endPoint = NetworkEndPoint.Parse(IP_ADDRESS, PORT);
+        SingularConnection = NetDrive.Connect(endPoints);
+        ServerActive = true;
+        EventListener();
+    }
+    public void StopServing()
+    {
+        if (ServerActive == true)
+        {
+            EventDestroyer();
+            NetDrive.Dispose();
+            ServerActive = false;
+            SingularConnection = default(NetworkConnection);
+        }
+    }
+    public void OnDestroy()
+    {
+        StopServing();
+    }
+    public void Update()
+    {
+        if (ServerActive == false)
+        {
+            return;
+        }
+        NetDrive.ScheduleUpdate().Complete();
+        CheckAlive();
+        UpdateMsgPipe();
+    }
+    private void CheckAlive()
+    {
+        if (!SingularConnection.IsCreated && isActive)
+        {
+            Debug.Log("[SERVER ERROR] Connection Lost ;(");
+            Drop?.Invoke();
+            StopServing();
+        }
+    }
+    private void UpdateMsgPipe()
+    {
+        DataStreamReader stream;
+        NetworkEvent.Type command;
+        while ((command = SingularConnection.PopEvent(NetDrive, out stream)) != NetworkEvent.Type.Empty) ;
+        {
+            if (command == NetworkEvent.Type.Connect)
+            {
+                SendToServer(new NetWelcome());
+            }
+            else if (command == NetworkEvent.Type.Data)
+            {
+                NetUtility.OnData(stream, default(NetworkConnection));
+            }
+            else if (command == NetworkEvent.Type.Disconnect)
+            {
+                Debug.Log("[SERVER] Client has disconnected");
+                SingularConnection = default(NetworkConnection);
+                ConnectionDrop?.Invoke();
+                StopServing();
+            }
+        }
+    }
+    public void SendToServer(NetMessage message)
+    {
+        DataStreamWriter writer;
+        NetDrive.BeginSend(SingularConnection, out writer);
+        // message.Serialize(ref writer);
+        driver.EndSend(writer);
+    }
+    private void EventListener()
+    {
+        // NetUtility.KeepConnectionStable += OnKeepAlive;
+    }
+    private void EventDestroyer()
+    {
+        // NetUtility.KeepConnectionStable -= OnKeepAlive;
+    }
+    private void OnKeepAlive(NetMessage message)
+    {
+        SendToServer(message);
     }
 }
+
+/*
+Fotis, just if you are confused, when calling a function I also check if it returns null : 
+
+    <functionName?.Invoke()>
+                ^^^
+    This is used to check if it is null, or the driver would stop communicating.
+*/
--- Assets/Scripts/Server/Server2.cs
+++ Assets/Scripts/Server/Server2.cs
@@ -1,28 +1,141 @@
+<<<<<<< Updated upstream
 /* using System.Collections;
+=======
+using System;
+using System.Collections;
+>>>>>>> Stashed changes
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Networking.Transport;
 using UnityEngine;
 
 public class Server2 : MonoBehaviour
 {
     public static Server2 Instance { set; get; }
-
     private void Awake()
     {
         Instance = this;
     }
     public NetworkDriver NetDrive;
+<<<<<<< Updated upstream
     private List<NetworkConnection> Connections;
+=======
+    private NativeList<NetworkConnection> Connections;
+>>>>>>> Stashed changes
     private bool ServerActive = false;
     private const float Delay = 20f;
     private float LastDelay;
     public System.Action<string> Drop;
     // [SerializeField] int PORT = 8000;
-    public void Init(ushort PORT)
+
+    public void __init__(ushort PORT)
     {
         NetDrive = NetworkDriver.Create();
         NetworkEndPoint endPoint = NetworkEndPoint.AnyIpv4;
-        NetDrive.Port = PORT;
-        NetDrive.Bind(endPoint);
+        endPoint.Port = PORT;
+
+        if (NetDrive.Bind(endPoint) != 0)
+        {
+            Debug.Log($"[SERVER ERROR] Unable to reach port {endPoint.Port}");
+            return;
+        }
+        else
+        {
+            NetDrive.Listen();
+            Debug.Log($"[SERVER] Listening to port {endPoint.Port}");
+        }
+
+        Connections = new NativeList<NetworkConnection>(2, Allocator.Persistent); // Max amount is 2
+        ServerActive = true;
     }
+<<<<<<< Updated upstream
 }*/
+=======
+    public void StopServing()
+    {
+        if (ServerActive == true)
+        {
+            NetDrive.Dispose();
+            Connections.Dispose();
+            ServerActive = false;
+        }
+    }
+    public void OnDestroy()
+    {
+        StopServing();
+    }
+    public void Update()
+    {
+        if (ServerActive == false)
+        {
+            return;
+        }
+        // ServerStatusSend();
+
+        NetDrive.ScheduleUpdate().Complete();
+        DestroyBadServer();
+        GetConnection();
+        UpdateMsgPipe();
+    }
+    private void DestroyBadServer()
+    {
+        for (int i = 0; i < Connections.Length; i++)
+        {
+            if (!Connections[i].IsCreated)
+            {
+                Connections.RemoveAtSwapBack(i);
+                --i;
+            }
+        }
+    }
+    private void GetConnection()
+    {
+        NetworkConnection NetConnection;
+        while ((NetConnection = NetDrive.Accept()) != default(NetworkConnection))
+        {
+            Connections.Add(NetConnection);
+        }
+    }
+    private void UpdateMsgPipe()
+    {
+        DataStreamReader stream;
+        for (int i = 0; i < Connections.Length; i++)
+        {
+            NetworkEvent.Type command;
+            while ((command = NetDrive.PopEventForConnection(Connections[i], out stream)) != NetworkEvent.Type.Empty)
+            {
+                if (command == NetworkEvent.Type.Data)
+                {
+                    // NetUtility.OneData(stream, Connections[i], this);
+                }
+                else if (command == NetworkEvent.Type.Disconnect)
+                {
+                    Debug.Log("[SERVER] Client disconnection");
+                    Connections[i] = default(NetworkConnection);
+                    Drop?.Invoke();
+                    StopServing();
+                }
+            }
+        }
+    }
+    public void SendToClient(NetworkConnection currentConnection, NetMessage message)
+    {
+        DataStreamWriter writer;
+        NetDrive.BeginSend(currentConnection, out writer);
+        message.Serialize(ref writer);
+        driver.EndSend(writer);
+    }
+    public void BroadCast(NetMessage message)
+    {
+        for (int i = 0; i < Connections.Length; i++)
+        {
+            if (Connections[i].IsCreated)
+            {
+                // Debug.Log($"[SERVER] Sending {message.Code} to {Connections[i].InternalId}");
+                SendToClient(Connections[i], msg);
+            }
+        }
+    }
+
+}
+>>>>>>> Stashed changes
